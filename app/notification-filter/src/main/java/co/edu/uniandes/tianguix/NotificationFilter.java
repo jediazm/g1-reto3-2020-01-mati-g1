6eae329252a54ccf5e7ee50e647e33f973ba15e1
Daniel Bellón <daniel.bellon@payulatam.com>
2020-04-26 17:21:13 -0500

    add credentials to rabbit mq in docker-compose.yml

---
+++ app/notification-filter/src/main/java/co/edu/uniandes/tianguix/NotificationFilter.java
@@ -0,0 +1,18 @@
+package co.edu.uniandes.tianguix;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:d.bellonc@uniandes.edu.co"> Daniel Bellón </a>
+ * @since 0.0.1
+ */
+@Slf4j
+@SpringBootApplication
+public class NotificationFilter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationFilter.class, args);
+	}
+}
